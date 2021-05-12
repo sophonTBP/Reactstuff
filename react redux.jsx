e4fe929625d3e6f201fb0fb2f45f7c224e7f9cee
Lucas Melin <65817583+sophonTBP@users.noreply.github.com>
2021-05-12 19:24:52 +0200

    exos 1205

--- react redux.jsx
+++ react redux.jsx
@@ -25,6 +25,21 @@ class DisplayMessages extends React.Component {
 
 
 
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
   /////////// Manage State Locally First----------
   /////Missed the indexing of the data-------------
   class DisplayMessages extends React.Component {
@@ -100,3 +115,173 @@ const messageReducer = (state = [], action) => {
 };
 
 const store = Redux.createStore(messageReducer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////// a submit button that makes a list with inputs great for todo lists..........
+// Redux:
+const ADD = 'ADD';
+
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message
+  }
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+
+
+const store = Redux.createStore(messageReducer);
+
+// React:
+
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {  
+    this.setState((state) => {
+      const currentMessage = state.input;
+      return {
+        input: '',
+        messages: state.messages.concat(currentMessage)
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+
+const Provider = ReactRedux.Provider;
+
+class AppWrapper extends React.Component {
+  // Render the Provider below this line
+
+render(){return(
+  <Provider store={store}>
+  <DisplayMessages/>
+</Provider>
+)}
+  // Change code above this line
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////mapping state to props.?!??...
+
+
+
+const state = [];
+
+// Change code below this line
+let mapStateToProps = (state) => {return{
+
+messages : state}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////map dispatch to props
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// Change code below this line
+
+const mapDispatchToProps = (dispatch)=>{
+  return{
+    submitNewMessage :function(message) {
+   
+    
+     dispatch(addMessage(message))
+  }
+}}
+
